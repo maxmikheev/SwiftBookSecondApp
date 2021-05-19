1d09f719bd939e4c282fb8d3aaad93a62ca37d5c
Maksim Mikheev <MMikheev@yotateam.com>
2021-05-19 20:15:59 +0300

    Создал "приложение" с экраном светофора. Выставил констрейнты, сделал вью как можно более круглыми.

--- SwiftBookSecondApp/ViewController.swift
+++ SwiftBookSecondApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = redLightView.bounds.height / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.bounds.height / 2
+        greenLightView.layer.cornerRadius = greenLightView.bounds.height / 2
+        
     }
-
-
+    
 }
 
