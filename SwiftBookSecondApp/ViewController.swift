6c4755ae3e8feb1080ffbf294f5ab38b2da6bdd8
Maksim Mikheev <MMikheev@yotateam.com>
2021-05-19 19:00:29 +0300

    Initial Commit

---
+++ SwiftBookSecondApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBookSecondApp
+//
+//  Created by Максим Михеев on 19.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
